bd20f561b48c4d5e2aa2d46f4d29e6602d3867fb
Elves Brito <elvesbd@yahoo.com>
2021-09-01 20:11:25 -0300

    feat: created component home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
 import Head from 'next/head';
 
+import { SubscribeButton } from '../components/SubscribeButton';
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home</title>
       </Head>
-      <h1>Hello World!</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React </span>world.
+          </h1>
+          <p>
+            Get acess to all the publication <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/girl.svg" alt="girl coding" />
+      </main>
     </>
   );
 }
