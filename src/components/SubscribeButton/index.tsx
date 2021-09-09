9a53776ec2bf483fb8c16b7fb72077dd74f4c7fe
Elves Brito <elvesbd@yahoo.com>
2021-09-09 20:39:10 -0300

    chore: connect faunaDB and create stripe checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,17 @@
+import { signIn, useSession } from 'next-auth/client';
+
 import styles from './style.module.scss';
 
 export function SubscribeButton() {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+  }
+
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
