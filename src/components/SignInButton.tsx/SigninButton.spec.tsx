8ef4c3402851e2257bcdbe40869a69f72e10e3a9
elvesbd <elvesbd@yahoo.com>
2022-06-16 11:52:28 -0300

    test: create SigninButton component

---
+++ src/components/SignInButton.tsx/SigninButton.spec.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import { mocked } from 'jest-mock';
+import { useSession } from 'next-auth/client';
+
+import { SignInButton } from '.';
+
+jest.mock('next-auth/client');
+const useSessionMocked = mocked(useSession);
+
+describe('SigninButton component', () => {
+  it('renders correctly when user is not logged in', () => {
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    const { debug } = render(<SignInButton />);
+    expect(screen.getByText('Sign in with Github')).toBeInTheDocument();
+  });
+
+  it('renders correctly when user is logged in', () => {
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: { name: 'John doe', email: 'john@example.com' },
+        expires: 'fake-expires',
+      },
+      true,
+    ]);
+
+    const { debug } = render(<SignInButton />);
+    expect(screen.getByText('John doe')).toBeInTheDocument();
+  });
+});
